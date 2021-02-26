624c5e7e01bf27d693c595cec06c79380cf4af49
Junaid <Junaid.562000@gmail.com>
2021-02-26 11:02:25 +0530

    javascript functionality added

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
-fetch('https://api.quotable.io/random')
-  .then((res) => res.json())
-  .then((data) => console.log(data.content));
+// DOM Selectors
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loading
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 //GET Quote
-
 async function getQuote() {
+  loading();
   const apiUrl = 'https://api.quotable.io/random';
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data);
+    if (data.author === '') {
+      quoteText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.author;
+    }
+    console.log(data.content.length);
+    if (data.content.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.content;
+    // Stop loader , show quote
+
+    complete();
   } catch (err) {
     console.log('whoops !!! no quotes');
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+// On Load
+getQuote();
